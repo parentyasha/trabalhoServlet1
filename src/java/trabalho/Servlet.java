9f411b1f826e1fba94da59ee34b4c662310758ed
Aline Sanches <sanches.aline6@gmail.com>
2019-08-30 16:48:40 -0300

    Projeto criado

---
+++ src/java/trabalho/Servlet.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aluno
+ */
+@WebServlet(name = "Servlet", urlPatterns = {"/Servlet"})
+public class Servlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("image/jpeg;charset=UTF-8");
+        
+        String nome = request.getParameter("nome");
+        
+       
+            try(ServletOutputStream out = response.getOutputStream())
+            {
+                String caminho = getServletContext().getRealPath("/WEB-INF/"+nome+".jpg");
+                File f = new File(caminho);
+                FileInputStream fluxo = new FileInputStream(f);
+                byte dados[] = new byte[(int)f.length()];
+
+                fluxo.read(dados);
+                out.write(dados);
+            
+            }
+        
+        
+        
+//        try (ServletOutputStream out = response.getOutputStream())
+//        {
+//            out.print(getServletContext().getRealPath("/WEB-INF/xicara.png"));
+//        }
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet Servlet</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<p>Seu navegador é: </p>");
+//            String navegador = request.getHeader("User-Agent");
+//            out.print("<p>" + navegador + "</p>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+
+    
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/trabalho/Usuario.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho;
+
+/**
+ *
+ * @author aline
+ */
+public class Usuario 
+{
+    private String nome;
+    private String senha;
+    private String login;
+    private String foto;
+
+    public Usuario()
+    {
+        
+    }
+    public Usuario(String nome, String senha, String login, String foto) {
+        this.nome = nome;
+        this.senha = senha;
+        this.login = login;
+        this.foto = foto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+    
+    
+}
